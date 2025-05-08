e38719ace3ebaee10e7ccdf7622a1924157714dc
jiheon <wwlgjs77@gmail.com>
2025-05-08 11:17:08 +0900

    ..

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return;
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">🎉 OpenChat에 오신 걸 환영합니다!</h1>
+    </main>
+  );
 }
