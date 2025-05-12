9ddd507c48b616d1edb4d598d5f7e368a247912e
jiheon <wwlgjs77@gmail.com>
2025-05-12 14:05:36 +0900

    ..

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,20 @@
+// next.config.ts (혹은 next.config.js)
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   srcDir: "src",
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: "http://34.225.172.232:3000/auth/:path*",
+      },
+      {
+        source: "/api/chat/:path*",
+        destination: "http://34.225.172.232:3000/chat/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
